ee81c15247b671cde676863b13ae6bc7a7732b0e
Maurits van Rees <maurits@vanrees.org>
2013-11-17 13:54:13 +0100

    Add some further ideas.

--- collective/storedtranslations/registrycatalog.py
+++ collective/storedtranslations/registrycatalog.py
@@ -14,12 +14,28 @@ class RegistryCatalog(object):
         self.language = language
 
     def queryMessage(self, msgid, default=None):
+        # Note: msgid may be a unicode or an instance of
+        # zope.i18nmessageid.message.Message.  In the last case it may
+        # have its own default.
+        #default = getattr(msgid, 'default', default)
         registry = queryUtility(IRegistry, None)
         if registry is None:
             return default
+        # Note: there is no need to make a unicode of the Message when
+        # querying the registry.
         try:
             msgstr = registry[REGISTRY_BASE][self.domain][self.language][msgid]
         except KeyError:
+            # Options, possibly based on environment variables or
+            # registry settings:
+            # - return default
+            # - return formatted string with domain and msgid.
+            #   Do this in a different catalog?
+            # - show default of msgid if it is a Message.  This is
+            #   something else than the default of this method.
+            # - auto add msgid to the registry with an empty translation
+            #
+            #return u'[[{0}][{1}][[{2}]]'.format(self.domain, msgid, default)
             return default
         return msgstr
 
