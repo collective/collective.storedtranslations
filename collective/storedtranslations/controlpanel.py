84b65ad6ab81f8d3a4739ce3ff157d2b6b20e852
Maurits van Rees <maurits@vanrees.org>
2013-12-06 23:34:55 +0100

    Export one translation domain and language.

--- collective/storedtranslations/controlpanel.py
+++ collective/storedtranslations/controlpanel.py
@@ -1,5 +1,7 @@
+from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
+from StringIO import StringIO
 from collective.storedtranslations import _
 from collective.storedtranslations import DOMAINS
 from collective.storedtranslations import LANGUAGES
@@ -28,6 +30,7 @@ class ControlPanelFormWrapper(layout.FormWrapper):
 
     index = ViewPageTemplateFile('controlpanel_layout.pt')
 
+
 ControlPanelView = layout.wrap_form(ControlPanelForm, ControlPanelFormWrapper)
 ControlPanelView.label = _(u"Stored translation settings")
 
@@ -41,6 +44,7 @@ class TranslationDomainEditForm(form.EditForm):
     domain = u''
     language = u''
     control_panel_view = u'storedtranslations-controlpanel'
+    translation_export_view = u'storedtranslations-export'
 
     def update(self):
         self.domain = self.request.get('form.widgets.domain', '')
@@ -98,6 +102,22 @@ class TranslationDomainEditForm(form.EditForm):
             self.context.absolute_url(),
             self.control_panel_view))
 
+    @button.buttonAndHandler(_(u"Save and export translations"), name='export')
+    def handleExport(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        # First save the changes, just like the Save button.
+        self.applyChanges(data)
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"Changes saved and exported."),
+            "info")
+        view = '{}?form.widgets.domain={}&form.widgets.language={}'.format(
+            self.translation_export_view, self.domain, self.language)
+        self.request.response.redirect("%s/%s" % (
+            self.context.absolute_url(), view))
+
     def applyChanges(self, data):
         registry = getUtility(IRegistry)
         domain = data['domain']
@@ -120,3 +140,28 @@ class TranslationDomainEditForm(form.EditForm):
 TranslationDomainEdit = layout.wrap_form(TranslationDomainEditForm, ControlPanelFormWrapper)
 TranslationDomainEdit.label = _(u"Stored translation messages")
 TranslationDomainEdit.index = ViewPageTemplateFile('controlpanel_messages.pt')
+
+
+class TranslationExport(BrowserView):
+
+    def __call__(self):
+        self.domain = self.request.get('form.widgets.domain', '')
+        self.language = self.request.get('form.widgets.language', '')
+        if self.domain not in DOMAINS:
+            self.domain = ''
+        if self.language not in LANGUAGES:
+            self.language = ''
+        response = self.request.response
+        filename = '{}-{}.po'.format(self.domain, self.language)
+        response.setHeader('Content-type', 'text/x-gettext-translation')
+        response.setHeader('Content-disposition',
+                           'attachment; filename=%s' % filename)
+        registry = getUtility(IRegistry)
+        translations = registry[REGISTRY_BASE][self.domain][self.language]
+        out = StringIO()
+        for msgid, msgstr in translations.items():
+            out.write('msgid "%s"\n' % msgid)
+            # Avoid printing "None"
+            out.write('msgstr "%s"\n' % (msgstr or ''))
+            out.write('\n')
+        return out.getvalue()
